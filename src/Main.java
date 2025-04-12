bf6b23087f7c2d2820c2f51199fc292a1bec6436
Александр Жуков <a.zhukov@uzum.com>
2025-04-12 11:55:01 +0200

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello Sasha!");
+        int a = 2;
+        System.out.println(a);
     }
 }
